fbcfaf6576551320eceb7e2d9f474d938ddc549b
bkaoluz <bkaoluz@gmail.com>
2023-02-09 22:57:13 +0900

    df

---
+++ ryan/README.md
@@ -0,0 +1,3 @@
+ryonice site this is roine
+discord -- > 라오니#8469
+Luzo#3583
---
+++ ryan/support.js
@@ -0,0 +1,5 @@
+function su()
+{
+    alert("후원은 라오니 사이트를 만드신 개발자님에게 후원됩니다");
+    window.open("https://paypal.me/Fantboss");
+}
