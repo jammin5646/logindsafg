007206240896aa671908c046e2c7de1901eab3f0
bkaoluz <bkaoluz@gmail.com>
2023-02-10 03:46:51 +0900

    hfgh

--- login.js
+++ login.js
@@ -1,8 +1,8 @@
 function check(form)
 {
-    if(form.userid.value=="Luzo","luzo")
+    if(form.userid.value=="71005031","ryan","luzo")
     {
-        if(form.userpassword.value=="luzo")
+        if(form.userpassword.value=="491740","ryan","luzo")
         {
             alert("Login페이지를 닫아주세요!")
             window.open("ryan/index.html")
